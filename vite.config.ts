323b0155a185722139668dc6c9ba5f8f028c6ce5
BugbearR <bugbearr@bugbearr.jp>
2024-03-04 22:16:20 +0900

    Update Vite server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,24 @@
 import { defineConfig, UserConfig } from "vite";
 import fs from "fs";
 
-const defaultConfigOptions : UserConfig = {
-    server: {
-        port: 3000,
-        strictPort: true,
-        https: {
-            key: undefined,
-            cert: undefined,
+export default defineConfig((/*{ command, mode, isSsrBuild, isPreview }*/) => {
+    const defaultConfigOptions : UserConfig = {
+        server: {
+            port: 3000,
+            strictPort: true,
+            https: {
+                key: undefined,
+                cert: undefined,
+            },
         },
-    },
-};
+    };
 
-if (fs.existsSync("./cert/privkey.pem") && fs.existsSync("./cert/cert.pem")) {
-    defaultConfigOptions.server = defaultConfigOptions.server || {};
-    defaultConfigOptions.server.https = defaultConfigOptions.server.https || {};
+    if (fs.existsSync("./cert/privkey.pem") && fs.existsSync("./cert/cert.pem")) {
+        defaultConfigOptions.server = defaultConfigOptions.server || {};
+        defaultConfigOptions.server.https = defaultConfigOptions.server.https || {};
 
-    defaultConfigOptions.server.https.key = fs.readFileSync("./cert/privkey.pem");
-    defaultConfigOptions.server.https.cert = fs.readFileSync("./cert/cert.pem");
-}
-
-export default defineConfig(defaultConfigOptions);
+        defaultConfigOptions.server.https.key = fs.readFileSync("./cert/privkey.pem");
+        defaultConfigOptions.server.https.cert = fs.readFileSync("./cert/cert.pem");
+    }
+    return defaultConfigOptions;
+});
